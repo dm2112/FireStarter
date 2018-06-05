ad2ed58f2b196a266c771bf4595bb7dddd780cf7
dm2112 <damo6246@colorado.edu>
2018-06-05 15:42:59 -0400

    yuh

--- HouseParty/ViewController.swift
+++ HouseParty/ViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var signBtn: UIButton!
+    @IBOutlet weak var houseLbl: UILabel!
+    @IBOutlet weak var signUpView: UIView!
+    @IBOutlet weak var logBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func signUpB(sender: UIButton){
+        signBtn.isHidden = true
+        logBtn.isHidden = true
+        houseLbl.isHidden = true
+        signUpView.isHidden = false
     }
 
 
